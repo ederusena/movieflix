255dfc85cbc1a5c9b3d21716a95b5571ab9bbede
Eder Sena <edersena.ehs@motiva.com.br>
2025-12-30 19:17:31 -0300

    Add JSON date formatting to releaseDate in MovieRequest and MovieResponse DTOs

--- src/main/java/com/movieflix/dto/MovieRequest.java
+++ src/main/java/com/movieflix/dto/MovieRequest.java
@@ -3,13 +3,15 @@ package com.movieflix.dto;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Builder;
 
 @Builder
 public record MovieRequest(
         String title,
         String description,
-        LocalDate releaseDate,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") LocalDate releaseDate,
         Double rating,
         List<Long> categories,
         List<Long> streamings) {
--- src/main/java/com/movieflix/dto/MovieResponse.java
+++ src/main/java/com/movieflix/dto/MovieResponse.java
@@ -3,18 +3,20 @@ package com.movieflix.dto;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Builder;
 
 @Builder
 public record MovieResponse(
-                Long id,
-                String title,
-                String description,
-                LocalDate releaseDate,
-                Double rating,
-                List<CategoryResponse> categories,
-                List<StreamingResponse> streamings,
-                String createdAt,
-                String updatedAt) {
+        Long id,
+        String title,
+        String description,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") LocalDate releaseDate,
+        Double rating,
+        List<CategoryResponse> categories,
+        List<StreamingResponse> streamings,
+        String createdAt,
+        String updatedAt) {
 
 }
