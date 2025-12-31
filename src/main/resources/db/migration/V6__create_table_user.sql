d33131c0164ea9145cca3c7339bbb10c21c056b7
Eder Sena <edersena.ehs@motiva.com.br>
2025-12-31 10:44:19 -0300

    Add created_at and updated_at columns to users table

--- src/main/resources/db/migration/V6__create_table_user.sql
+++ src/main/resources/db/migration/V6__create_table_user.sql
@@ -2,5 +2,7 @@ CREATE TABLE users (
     id serial PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
 );
