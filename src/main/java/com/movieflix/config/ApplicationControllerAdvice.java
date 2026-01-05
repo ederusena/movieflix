95edaa2edc0fbf371be86ea3aba939fc3376681d
Eder Sena <edersena.ehs@motiva.com.br>
2026-01-05 09:13:00 -0300

    Add exception handling for MethodArgumentNotValidException in ApplicationControllerAdvice

--- src/main/java/com/movieflix/config/ApplicationControllerAdvice.java
+++ src/main/java/com/movieflix/config/ApplicationControllerAdvice.java
@@ -1,6 +1,11 @@
 package com.movieflix.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,5 +19,15 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleNotFoundException(UsernameOrPasswordInvalidException ex) {
         return ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            errors.put(((FieldError) error).getField(), error.getDefaultMessage());
+        });
+        return errors;
     }
 }
