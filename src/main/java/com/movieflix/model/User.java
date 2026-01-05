c4e16beddbe9cc45b7b050e069f72407311f79a0
Eder Sena <edersena.ehs@motiva.com.br>
2026-01-04 22:11:25 -0300

    Implement UserDetails methods in User class

--- src/main/java/com/movieflix/model/User.java
+++ src/main/java/com/movieflix/model/User.java
@@ -53,13 +53,31 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+        return java.util.List.of();
     }
 
     @Override
     public String getUsername() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
